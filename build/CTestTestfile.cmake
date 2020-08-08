cc2cc0751b99da025cc62661926574115858d31c
RamAIbot <ramachandranss22@gmail.com>
2020-08-08 08:13:54 +0000

    FinalVersion

--- build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/workspace/whereami/Udacity-WhereamI/src
-# Build directory: /home/workspace/whereami/Udacity-WhereamI/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(my_robot)
-subdirs(ball_chaser)
